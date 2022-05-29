761611fe9934cb511c2e37c31ca20e0a1f91630b
M0onkeyDLuffy <devrinokumura@gmail.com>
2022-05-29 12:16:19 +0100

    Update all_check.py
    Updating the def check_disk_full function because it was missing.

--- all_check.py
+++ all_check.py
@@ -1,4 +1,4 @@
-#!/usr/bin/#!/usr/bin/env python
+#!/usr/bin/env python
 
 import os
 import sys
@@ -6,6 +6,17 @@ import sys
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exist("/run/reboot-required")
+
+def check_disk_full(disk, min_absolute, min_percent):
+    """Return True if there isn't enought disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    #Calculate the precentage of the free space
+    percent_free = 100 * du.free / du.total
+    #Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return True
+    return False
 
 def main():
     if check_reboot():
